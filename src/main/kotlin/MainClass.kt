2d10b8600bd8bbd72ce5340dc40387ce59957f64
Ulyana <u.lyzhina@indriver.com>
2022-12-03 22:42:03 +0300

    lesson1-test2

--- src/main/kotlin/MainClass.kt
+++ src/main/kotlin/MainClass.kt
@@ -1,5 +1,10 @@
 class MainClass {
+    private val classNumber: Int = 20
     fun getLocalNumber(): Int {
         return 14;
+    }
+
+    fun getClassNumber(): Int{
+        return classNumber
     }
 }
--- src/test/kotlin/MainClassTest.kt
+++ src/test/kotlin/MainClassTest.kt
@@ -6,4 +6,9 @@ class MainClassTest {
     fun testGetLocalNumber() {
         Assert.assertTrue("Метод getLocalNumber не возвращает 14", MainClass().getLocalNumber() === 14)
     }
+
+    @Test
+    fun testGetClassNumber(){
+        Assert.assertTrue("Метод getClassNumber возвращает число < 45 или == 45", MainClass().getClassNumber() > 45)
+    }
 }
